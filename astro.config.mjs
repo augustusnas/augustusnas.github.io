f2ca15c187c7c448fb8e2fef245b069796eb29b1
Charles Nas-Omogiafo <cano@Charless-Mac-mini.local>
2026-04-14 19:20:29 +0100

    update site url to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://augustusnas.github.io',
+  site: 'https://augustusnas.com',
   vite: {
     plugins: [tailwindcss()],
   },
